818a033623085b9a66ff3aade28924d14c099aeb
perkinj <15077304004@163.com>
2018-04-22 15:25:58 +0800

    add redux and react-redux

--- src/app/components/Hello.tsx
+++ src/app/components/Hello.tsx
@@ -1,5 +1,30 @@
-import * as React from "react";
-import * as style from './style.css';
-export interface HelloProps { compiler: string; framework: string; }
+import * as React from 'react';
 
-export const Hello = (props: HelloProps) => <h1 className={style.color} >Hello111 from {props.compiler} and {props.framework}!</h1>;
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
+}
+
+export default function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
+  }
+
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
+      </div>
+    </div>
+  );
+}
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,13 @@
-import * as React from 'react';
-import {  Hello } from 'app/components/Hello';
+import * as React from "react";
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import { hot } from 'react-hot-loader';
+const store = configureStore();
+
 
 export const App = hot(module)(() => (
-  <Hello compiler="111" framework="222"/>
+    <Provider store={store}>
+        <Hello/>
+    </Provider>
 ));
